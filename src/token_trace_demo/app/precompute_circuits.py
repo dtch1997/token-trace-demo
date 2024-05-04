2d1a326109c9b1e74b278e22f65d3274553671fd
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-05-04 01:09:11 +0100

    add more prompts

--- src/token_trace_demo/app/precompute_circuits.py
+++ src/token_trace_demo/app/precompute_circuits.py
@@ -4,10 +4,10 @@ from token_trace_demo.app.load_data import DATA_DIR, hash_text
 PROMPTS = [
     # IOI
     "When John and Mary went to the shops, John gave the bag to Mary",
-    # "When Tim and Jane went to the shops, Tim gave the bag to Jane",
-    # # Factual recall
-    # "Fact: Tokyo is a city in the country of Japan",
-    # "Fact: Delhi is a city in the country of India",
+    "When Tim and Jane went to the shops, Tim gave the bag to Jane",
+    # Factual recall
+    "Fact: Tokyo is a city in the country of Japan",
+    "Fact: Delhi is a city in the country of India",
 ]
 
 if __name__ == "__main__":
